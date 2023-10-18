88efc3ed88e97f6f5291242bc80757c55e6c5bcb
Scott Rapson <scottapotamas@gmail.com>
2023-10-18 22:41:04 +1030

    STM32 Arduino GPIO test firmware

---
+++ firmware/gpio_tests/stm32duino/stm32duino.ino
@@ -0,0 +1,37 @@
+bool trigger_pending = false;
+
+void setup( void )
+{
+  // Output LED
+  pinMode(PB0, OUTPUT);
+
+  // Input IO
+  attachInterrupt( digitalPinToInterrupt(PA0), 
+                   externalTrigger, 
+                   RISING );
+}
+
+void externalTrigger( void )
+{
+  trigger_pending = true;
+}
+
+void loop( void )
+{
+
+      digitalWrite(PB0, HIGH);
+  delay(1);
+        digitalWrite(PB0, LOW);
+  delay(1);
+//
+//  if( trigger_pending )
+//  {
+//    digitalWrite(PB0, HIGH);
+//    trigger_pending = false;
+//  }
+//  else
+//  {
+//    digitalWrite(PB0, LOW);
+//  }
+
+}
